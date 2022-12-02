c14641108b3d778b03426e4da2a224a073ecba84
Lutz Mitländer <lutzmitlaender@gmx.net>
2022-12-02 17:38:16 +0100

    Add project files.

---
+++ Day_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Input.txt");
+string[] input = File.ReadAllLines(path);
+
+List<int> elfList = new List<int>();
+int elfCalories = 0;
+
+foreach (string line in input)
+{
+	if(!string.IsNullOrEmpty(line))
+	{
+		elfCalories += Convert.ToInt32(line);
+	}
+	else
+	{
+		elfList.Add(elfCalories);
+		elfCalories = 0;
+	}
+}
+elfList.Sort((a,b) => b.CompareTo(a));
+
+int combinedTop3 = 0;
+Console.WriteLine("Top 3 Elves are:");
+for(int i = 0; i < 3; i++)
+{
+	Console.WriteLine($"{i + 1}. Place: {elfList[i]} calories");
+	combinedTop3 += elfList[i];
+}
+Console.WriteLine($"Combined they have: {combinedTop3} calories");
---
+++ Day_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+const int ROCK = 1;
+const int PAPER = 2;
+const int SCISSORS = 3;
+const int LOSS = 0;
+const int DRAW = 3;
+const int WIN = 6;
+
+string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"input.txt");
+int totalPointsPartOne = 0;
+int totalPointsPartTwo = 0;
+
+foreach(string line in File.ReadLines(path))
+{
+	switch (line[0], line[2])
+	{
+		case ('A', 'X'):
+			totalPointsPartOne += DRAW + ROCK;
+			totalPointsPartTwo += LOSS + SCISSORS;
+			break;
+		case ('A', 'Y'):
+			totalPointsPartOne += WIN + PAPER;
+			totalPointsPartTwo += DRAW + ROCK;
+			break;
+		case ('A', 'Z'):
+			totalPointsPartOne += LOSS + SCISSORS;
+			totalPointsPartTwo += WIN + PAPER;
+			break;
+		case ('B', 'X'):
+			totalPointsPartOne += LOSS + ROCK;
+			totalPointsPartTwo += LOSS + ROCK;
+			break;
+		case ('B', 'Y'):
+			totalPointsPartOne += DRAW + PAPER;
+			totalPointsPartTwo += DRAW + PAPER;
+			break;
+		case ('B', 'Z'):
+			totalPointsPartOne += WIN + SCISSORS;
+			totalPointsPartTwo += WIN + SCISSORS;
+			break;
+		case ('C', 'X'):
+			totalPointsPartOne += WIN + ROCK;
+			totalPointsPartTwo += LOSS + PAPER;
+			break;
+		case ('C', 'Y'):
+			totalPointsPartOne += LOSS + PAPER;
+			totalPointsPartTwo += DRAW + SCISSORS;
+			break;
+		case ('C', 'Z'):
+			totalPointsPartOne += DRAW + SCISSORS;
+			totalPointsPartTwo += WIN + ROCK;
+			break;
+	}
+}
+
+Console.WriteLine($"Total Points according to strategy guide part 1: {totalPointsPartOne}");
+Console.WriteLine($"Total Points according to strategy guide part 2: {totalPointsPartTwo}");
